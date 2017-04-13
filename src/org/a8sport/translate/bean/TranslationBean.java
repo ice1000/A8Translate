593dbcfb1cc406ede0c1c6fe488b5e511ff09767
ice1000 <ice1000kotlin@foxmail.com>
2017-04-13 16:38:02 +0800

    expand json string, injecting language

--- src/org/a8sport/translate/bean/TranslationBean.java
+++ src/org/a8sport/translate/bean/TranslationBean.java
@@ -10,6 +10,7 @@ import java.util.List;
  * Created by Pinger on 2016/12/10.
  * 翻译结果javabean
  */
+@SuppressWarnings("WeakerAccess")
 public class TranslationBean {
 
     /**
@@ -23,13 +24,54 @@ public class TranslationBean {
     private final static int NO_RESULT = 60;   // 无词典结果
     public final static String EMPTY = "返回数据为空";
 
-    /**
+    /*
      * translation : ["解决"]
-     * basic : {"us-phonetic":"rɪ'zɑlv","phonetic":"rɪ'zɒlv","uk-phonetic":"rɪ'zɒlv","explains":["n. 坚决；决定要做的事","vt. 决定；溶解；使\u2026分解；决心要做\u2026","vi. 解决；决心；分解"]}
+     * basic :
+     */
+    /*
+    {
+      "us-phonetic": "rɪ'zɑlv",
+      "phonetic": "rɪ'zɒlv",
+      "uk-phonetic": "rɪ'zɒlv",
+      "explains": [
+        "n. 坚决；决定要做的事",
+        "vt. 决定；溶解；使\u2026分解；决心要做\u2026",
+        "vi. 解决；决心；分解"
+      ]
+    }
+    */
+    /*
      * query : resolve
      * errorCode : 0
-     * web : [{"value":["决心","决定","解决"],"key":"Resolve"},{"value":["归结为","使分解为","分解为"],"key":"resolve into"},{"value":["解决分歧","消除分歧"],"key":"resolve differences"}]
+     * web :
      */
+    /*
+    [
+      {
+        "value": [
+          "决心",
+          "决定",
+          "解决"
+        ],
+        "key": "Resolve"
+      },
+      {
+        "value": [
+          "归结为",
+          "使分解为",
+          "分解为"
+        ],
+        "key": "resolve into"
+      },
+      {
+        "value": [
+          "解决分歧",
+          "消除分歧"
+        ],
+        "key": "resolve differences"
+      }
+    ]
+    */
 
     public BasicBean basic;
     public String query;
@@ -37,14 +79,14 @@ public class TranslationBean {
     public List<String> translation;
     public List<WebBean> web;
 
-    class BasicBean {
+    public class BasicBean {
         public String usPhonetic;
         public String phonetic;
         public String ukPhonetic;
         public List<String> explains;
     }
 
-    class WebBean {
+    public class WebBean {
         public String key;
         public List<String> value;
     }
@@ -103,9 +145,7 @@ public class TranslationBean {
      */
     @Nullable
     private String getExplains() {
-        if (basic == null) {
-            return null;
-        }
+        if (basic == null) return null;
         StringBuilder result = null;
         List<String> explains = basic.explains;
         if (explains.size() > 0) {
@@ -122,9 +162,7 @@ public class TranslationBean {
      */
     @Nullable
     private String getTranslationResult() {
-        if (translation == null) {
-            return null;
-        }
+        if (translation == null) return null;
         StringBuilder result = null;
         if (translation.size() > 0) {
             result = new StringBuilder();
@@ -155,11 +193,8 @@ public class TranslationBean {
                 List<String> value = webBean.value;
                 for (int i = 0; i < value.size(); i++) {
                     String keyword = value.get(i);
-                    if (i < value.size() - 1) {
-                        result.append(keyword).append("，");
-                    } else {
-                        result.append(keyword);
-                    }
+                    if (i < value.size() - 1) result.append(keyword).append("，");
+                    else result.append(keyword);
                 }
 
                 result.append("\n");
@@ -187,36 +222,25 @@ public class TranslationBean {
             if (translation != null) {
                 translation = translation.substring(0, translation.length() - 1);
                 if (!translation.equals(query)) {
-                    if (isSentence()) {
-                        string = getTranslationResult() + "\n";
-                    } else {
-                        string = (query + "：" + getTranslationResult() + "\n");
-                    }
+                    if (isSentence()) string = getTranslationResult() + "\n";
+                    else string = (query + "：" + getTranslationResult() + "\n");
                 }
             }
             if (getPhonetic() != null) {
-                if (string == null) {
-                    string = "";
-                }
+                if (string == null) string = "";
                 string += (getPhonetic() + "\n");
             }
             if (getExplains() != null) {
-                if (string == null) {
-                    string = "";
-                }
+                if (string == null) string = "";
                 string += (getExplains());
             }
             if (getWebResult() != null) {
-                if (string == null) {
-                    string = "";
-                }
+                if (string == null) string = "";
                 string += "网络释义：\n";
                 string += (getWebResult());
             }
         }
-        if (string == null) {
-            string = "你选的内容：" + query + "\n抱歉,翻译不了...";
-        }
+        if (string == null) string = "你选的内容：" + query + "\n\n抱歉,翻译不了...";
         return string;
     }
 }
