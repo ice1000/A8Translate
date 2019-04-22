dd32684da1d023d052abf091081ef6031c80bc9d
Tesla_Ice_Zhang <ice1000kotlin@foxmail.com>
2019-04-22 10:56:17 -0400

    Fix #14

--- src/org/a8sport/translate/main/LocalData.kt
+++ src/org/a8sport/translate/main/LocalData.kt
@@ -16,11 +16,11 @@ const val PREFIX_NAME = "A8Translate"
 
 object LocalData {
 	private val p = Properties()
-	private val f = File(System.getProperty("user.home") + "/a8temp.properties")
+	// private val f = File(System.getProperty("user.home") + "/a8temp.properties")
 
 	init {
-		if (!f.exists()) f.createNewFile()
-		p.load(FileReader(f))
+		// if (!f.exists()) f.createNewFile()
+		// p.load(FileReader(f))
 	}
 
 	fun store(@NonNls key: String, @NonNls value: String) {
@@ -33,7 +33,7 @@ object LocalData {
 		save()
 	}
 
-	private fun save() = p.store(FileWriter(f), "Created by a8translate")
+	private fun save() = Unit // p.store(FileWriter(f), "Created by a8translate")
 
 	fun read(@NonNls key: String): String? = p.getProperty(StringUtils.uncapitalize(key))
 }
